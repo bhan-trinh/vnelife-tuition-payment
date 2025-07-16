2a4852753b8cdd6d3d7417f45fae3e2780524fe7
bhan-trinh <baohanmi@gmail.com>
2025-07-16 11:13:06 +0700

    Add images, fonts, colors assets

---
+++ src/assets/colors/theme.ts
@@ -0,0 +1,8 @@
+export const theme = {
+  colors: {
+    primary: '#e2623eff',
+    secondary: '#ffd754ff',
+    highlight1: '#d3441dff',
+    highlight2: '#ffbe19ff',
+  },
+};
---
+++ src/assets/imgComponents/imgComponents.tsx
@@ -0,0 +1,88 @@
+import {Image} from 'react-native';
+
+export const tuitionServiceImages = {
+  hocmai: (
+    <Image
+      style={{flex: 1, height: '100%', width: '100%'}}
+      source={require('../../assets/images/logos/hocmai.png')}
+      resizeMode="contain"
+    />
+  ),
+  vnedu: (
+    <Image
+      style={{flex: 1, height: '100%', width: '100%'}}
+      source={require('../../assets/images/logos/vnedu.png')}
+      resizeMode="contain"
+    />
+  ),
+  hocphi: (
+    <Image
+      style={{flex: 1, height: '100%', width: '100%'}}
+      source={require('../../assets/images/logos/hocmai.png')}
+      resizeMode="contain"
+    />
+  ),
+  bmc: (
+    <Image
+      style={{flex: 1, height: '100%', width: '100%'}}
+      source={require('../../assets/images/logos/hocmai.png')}
+      resizeMode="contain"
+    />
+  ),
+  ssc: (
+    <Image
+      style={{flex: 1, height: '100%', width: '100%'}}
+      source={require('../../assets/images/logos/ssc.jpg')}
+      resizeMode="contain"
+    />
+  ),
+  vinaid_card: (
+    <Image
+      style={{flex: 1, height: '100%', width: '100%'}}
+      source={require('../../assets/images/logos/vinaIDcard.png')}
+      resizeMode="contain"
+    />
+  ),
+  vinaid: (
+    <Image
+      style={{flex: 1, height: '100%', width: '100%'}}
+      source={require('../../assets/images/logos/vinaID.png')}
+      resizeMode="contain"
+    />
+  ),
+  edulink: (
+    <Image
+      style={{flex: 1, height: '100%', width: '100%'}}
+      source={require('../../assets/images/logos/edulinkcyc.png')}
+      resizeMode="contain"
+    />
+  ),
+  thu_phi_dh_cd: (
+    <Image
+      style={{flex: 1, height: '100%', width: '100%'}}
+      source={require('../../assets/images/logos/hocmai.png')}
+      resizeMode="contain"
+    />
+  ),
+  nop_phi_xet_tuyen_dh: (
+    <Image
+      style={{flex: 1, height: '100%', width: '100%'}}
+      source={require('../../assets/images/logos/hocmai.png')}
+      resizeMode="contain"
+    />
+  ),
+  dtsoft: (
+    <Image
+      style={{flex: 1, height: '100%', width: '100%'}}
+      source={require('../../assets/images/logos/dtsoft.jpg')}
+      resizeMode="contain"
+    />
+  ),
+  asc: (
+    <Image
+      style={{flex: 1, height: '100%', width: '100%'}}
+      source={require('../../assets/images/logos/hocmai.png')}
+      resizeMode="contain"
+    />
+  ),
+};
